fd5a89170368cdc121b68d80184a8c6f9783cc5a
john@andersoninnovative.com <john@andersoninnovative.com@120867ed-ad96-fafc-8a43-e235d9f8a922>
2011-08-31 14:48:39 +0000

    Tagged 'demo1' which is running on hosted demo site.

---
+++ tags/demo1/sptest/saml2sp/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+from views import sso_response, sso_test, sso_idp_select
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^sptest/', include('sptest.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+
+    (r'^sso/test/$', sso_test),
+    (r'^sso/idpselect/$', sso_idp_select),
+    (r'^sso/post/response/$', sso_response),
+)
---
+++ tags/demo1/sptest/saml2sp/views.py
@@ -0,0 +1,116 @@
+from xml.dom.minidom import parseString
+from django import forms
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_view_exempt
+import codex
+import saml2sp_settings
+
+
+#TODO: Pull IDP choices from a model. For now, just use the one from the settings.
+IDP_CHOICES = (
+    (saml2sp_settings.IDP_REQUEST_URL, saml2sp_settings.IDP_REQUEST_URL),
+)
+class IdpSelectionForm(forms.Form):
+    idp = forms.ChoiceField(choices=IDP_CHOICES)
+
+def _get_email_from_assertion(assertion):
+    """ Returns the email out of the assertion. """
+    #TODO: Wrangle with XML. Ugh.
+    return "dummy@dummy.com"
+
+def _email_to_username(email):
+    """
+    Returns a Django-safe username based on the email address.
+    This is because Django doesn't support email addresses as usernames yet.
+    """
+    return email.replace('@', 'AT')
+
+def _get_user_from_assertion(assertion):
+    """
+    Gets info out of the assertion and locally logs in this user.
+    May create a local user account first.
+    Returns the user object that was created.
+    """
+    email = _get_email_from_assertion(assertion)
+    try:
+        user = User.objects.get(email = email)
+    except:
+        user = User.objects.create_user(
+            _email_to_username(email),
+            email,
+            saml2sp_settings.SAML_USER_PASSWORD
+        )
+    #NOTE: This next line will fail if the user has changed his password
+    #      via the local account. This actually is a good thing, I think.
+    user = authenticate(username=user.username,
+                        password=saml2sp_settings.SAML_USER_PASSWORD)
+    return user
+
+def sso_login(request, request_url):
+    """
+    Replies with an XHTML SSO Request.
+    """
+    sso_destination = request.GET.get('next', None)
+    request.session['sso_destination'] = sso_destination
+    request = 'TODO'
+    token = sso_destination
+    tv = {
+        'request_url': request_url, #saml2sp_settings.IDP_REQUEST_URL,
+        'request': request,
+        'token': token,
+        'next': sso_destination,
+    }
+    return render_to_response('saml2sp/sso_post_request.html', tv)
+
+def sso_idp_select(request):
+    """
+    Allows the user to select an IDP.
+    """
+    if request.method == 'POST':
+        form = IdpSelectionForm(request.POST)
+        if form.is_valid():
+            idp_request_url = form.cleaned_data['idp']
+            return sso_login(request, idp_request_url)
+    else:
+        form = IdpSelectionForm()
+    tv = {
+        'form': form,
+    }
+    return render_to_response('saml2sp/sso_idp_selection.html', tv,
+        context_instance=RequestContext(request))
+
+
+@csrf_view_exempt
+def sso_response(request):
+    """
+    Handles a POSTed SSO Assertion and logs the user in.
+    """
+    #TODO: Only allow this view to accept POSTs from trusted sites.
+    #sso_session = request.session.get('sso_destination', None),
+    sso_session = request.POST.get('RelayState', None)
+    data = request.POST.get('SAMLResponse', None)
+    assertion = codex.decode_base64_and_inflate(data)
+    user = _get_user_from_assertion(assertion)
+    login(request, user)
+    tv = {
+        'user': user,
+        'assertion': assertion,
+        'sso_destination': sso_session,
+    }
+    return render_to_response('saml2sp/sso_complete.html', tv)
+
+@login_required
+def sso_test(request):
+    """
+    Exposes a simple resource that requires authentication,
+    so that we can kick-off the SAML conversation.
+    """
+    tv = {
+        'session': request.session,
+    }
+    return render_to_response('saml2sp/sso_test.html', tv)
