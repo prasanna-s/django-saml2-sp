6fd1e4f1a404e01a88dc1a6372306f46e094f919
john@andersoninnovative.com <john@andersoninnovative.com@120867ed-ad96-fafc-8a43-e235d9f8a922>
2012-05-12 01:19:25 +0000

    Tagging demo2; now supports proper SAML AuthnRequest and signing.

---
+++ tags/demo2/sptest/saml2sp/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls.defaults import *
+from views import sso_response, sso_test, sso_idp_select, sso_single_logout
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^sptest/', include('sptest.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+
+    (r'^test/$', sso_test),
+    (r'^idpselect/$', sso_idp_select),
+    (r'^acs/$', sso_response),
+    url(r'^singlelogout/$', sso_single_logout, name='sso_single_logout'),
+)
